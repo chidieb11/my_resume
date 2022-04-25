934eab0b4cebce795570e4117edf00c8ba6d9117
chidieb11 <chukwuchidieberejohn@gmail.com>
2022-04-25 12:38:06 -0700

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
 import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Education from "./pages/education/Education";
 import Experience from "./pages/experience/Experience";
 import Home from "./pages/home/Home";
 
 const App = () => {
   return (
-    <BrowserRouter>
+    <div>
       <Routes>
         <Route exact path="/" element={<Home />} />
         <Route path="education" element={<Education />} />
         <Route path="experience" element={<Experience />} />
       </Routes>
-    </BrowserRouter>
+    </div>
   );
 };
 
