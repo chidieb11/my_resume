2dc537c4cea32289ea7b70a29cac6254879c8823
Chukwu Chidiebere John <chukwuchidieberejohn@gmail.com>
2022-04-26 20:41:07 -0700

    Initial commit

--- src/pages/experience/Experience.jsx
+++ src/pages/experience/Experience.jsx
@@ -1,36 +1,28 @@
 import React from "react";
-import Footer from "../../components/footer/Footer";
-import Navbar from "../../components/navbar/Navbar";
-import ExperienceImage from "./../../resources/images/experience.svg";
-import ByteworksBadge from "./../../resources/images/byteworks.jpg";
 import "./experience.css";
+import Img from "../../resources/images/experience.svg";
+import Byteworks from "../../resources/images/byteworks.jpg";
+import Navbar from "../../components/navbar/Navbar";
+import Footer from "../../components/footer/Footer";
 
 const Experience = () => {
   return (
     <div>
       <Navbar />
       <div className="experienceContainer scale-in-center">
-        <div className="personalDetails">
-          <img
-            className="experienceImage"
-            src={ExperienceImage}
-            alt="educational image"
-          />
-          <div className="experienceDetails">
-            <h3 className="experienceHeading">Internship 💻</h3>
-            <p className="experienceSubHeading">Junior Fullstack Developer</p>
-            <p className="qualification">
-              I interned at Byteworks Technology as a junior fullstack developer
-              for a duration of four months. During my time there, I contributed
-              to the building of few highly scalable solutions where I was
-              tasked with the responsibility of ensuring the solutions passed
-              end to end testing before they are deployed to production.
-            </p>
-            <img
-              className="byteworks_badge"
-              src={ByteworksBadge}
-              alt="byteworks badge"
-            />
+        <img className="experienceImg" src={Img} alt="computer person" />
+        <div className="experienceInfo">
+          <h2 className="experienceHeader">Internsip 💻</h2>
+          <h4 className="experienceSubHeader">Junior Fullstack Developer</h4>
+          <p className="experienceDetails">
+            I interned at Byteworks Technology as a junior fullstack developer
+            for a duration of four months. During my time there, I contributed
+            to the building of few highly scalable solutions where I was tasked
+            with the responsibility of ensuring the solutions passed end to end
+            testing before they are deployed to production.
+          </p>
+          <div className="experienceImage">
+            <img className="byteworks" src={Byteworks} alt="" />
           </div>
         </div>
       </div>
